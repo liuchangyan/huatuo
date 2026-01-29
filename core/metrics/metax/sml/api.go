b19e6b7e1dd548652a25add239623dee461c314e
Teresaliu <teresaliu@didiglobal.com>
2026-01-29 02:39:12 -0500

    metax: add copyright message

--- core/metrics/metax/sml/api.go
+++ core/metrics/metax/sml/api.go
@@ -1,4 +1,5 @@
-// Copyright 2025 The HuaTuo Authors
+// Copyright 2026 The HuaTuo Authors
+// Copyright 2026 The MetaX Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
